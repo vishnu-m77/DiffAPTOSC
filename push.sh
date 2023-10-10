8bcda69f5876eace8ffbebd8be8c38525fa80a2c
vishnu-m77 <vmugunda@uwaterloo.ca>
2023-10-10 00:27:31 -0400

    Input_args

---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git add run.sh
+git add push.sh
+# git add A1Q1_AMATH_495.ipynb
+git add main.py
+git add README.md
+git add param/params.json
+git add requirements.txt
+git add .gitignore
+git commit -m "$1"
+git push
+echo "Done."
--- run.sh
+++ run.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-git add run.sh
-# git add A1Q1_AMATH_495.ipynb
-git add main.py
-git add README.md
-git add param/params.json
-git add requirements.txt
-git add .gitignore
-git commit -m "$1"
-git push
-echo "Done."
+python3 main.py
