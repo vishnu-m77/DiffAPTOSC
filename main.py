68f310becfdcf6c1c396b5e99ade30e21ae72769
Lakshay <59942900+Lakshay-G@users.noreply.github.com>
2023-11-18 15:39:57 -0500

    Pushing reverse diffusion branch

--- main.py
+++ main.py
@@ -29,43 +29,47 @@ import src.diffusion as diffusion
 if os.path.exists('project.log'):
     os.remove('project.log')
 
-logging.basicConfig(filename='project.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(filename='project.log', filemode='a',
+                    format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 logging.warning('This will get logged to a file')
 
 if __name__ == '__main__':
-    
+
     # Command line arguments
     parser = argparse.ArgumentParser(description='DiffMIC')
-    
+
     # Default values of parameters are defined
-    parser.add_argument('--param', default = 'param/params.json', help='file containing hyperparameters')
-    parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
-    parser.add_argument('-d', '--dataset', default = 'aptos', type = str, help='dataset')
-    
+    parser.add_argument('--param', default='param/params.json',
+                        help='file containing hyperparameters')
+    parser.add_argument('-v', '--verbose',
+                        help='increase output verbosity', action='store_true')
+    parser.add_argument('-d', '--dataset', default='aptos',
+                        type=str, help='dataset')
+
     args = parser.parse_args()
     verbose = args.verbose
     data = args.dataset
-    
+
     # Hyperparameters from json file
     with open(args.param) as paramfile:
         param = json.load(paramfile)
-    
+
     params = param[data]
-    MODEL_VERSION_DIR = "diffmic_conditional_results/" + str(params['N_STEPS']) + "steps/nn/" + str(params["RUN_NAME"]) + "/" + str(params["PRIOR_TYPE"]) + str(params["CAT_F_PHI"]) + "/" + str(params["F_PHI_TYPE"])
+    MODEL_VERSION_DIR = "diffmic_conditional_results/" + str(params['N_STEPS']) + "steps/nn/" + str(
+        params["RUN_NAME"]) + "/" + str(params["PRIOR_TYPE"]) + str(params["CAT_F_PHI"]) + "/" + str(params["F_PHI_TYPE"])
     params["MODEL_VERSION_DIR"] = MODEL_VERSION_DIR
     # logging.debug('verbose is {}'.format(verbose))
     if verbose:
         logging.info('params are {}'.format(params))
         # print(params)
-    
-    
+
     # Creates a report file
     report_file = 'report.txt'
-    
+
     data = dataloader.DataProcessor(param)
     train_loader, test_loader = data.get_dataloaders()
-    
+
     dcg_params = param["dcg"]
     dcg = dcg_module.DCG(dcg_params)
     dcg_module.train_DCG(dcg, param, train_loader, test_loader)
@@ -79,21 +83,44 @@ if __name__ == '__main__':
     #     y_fusions.append(y_fusion)
     #     y_locals.append(y_local)
     #     y_globals.append(y_global)
-        # logging.info(y_global)
-    
+    # logging.info(y_global)
+
     logging.info("DCG completed")
-    
+
     diffusion_config = param['diffusion']
     if verbose:
         logging.info("Diffusion model parameters: {}".format(diffusion_config))
-    FD = ForwardDiffusion(config=diffusion_config) # initialize class
+    FD = ForwardDiffusion(config=diffusion_config)  # initialize class
     # forward diffusion EXAMPLE call below where the parameters are explained in difusion.py script
-    noised_var = FD.forward(var = torch.tensor(0.0), prior = torch.tensor(0))
+    noised_var = FD.forward(var=torch.tensor(0.0), prior=torch.tensor(0))
     logging.info("Noised Variable is {}".format(noised_var))
-    
-    
+
+    #################### Reverse diffusion code begins #############################
+
+    for epoch in range(0, 5):
+
+        data_start = time.time()
+        data_time = 0
+
+        for i, feature_label_set in enumerate(train_loader):
+
+            x_batch, y_labels_batch = feature_label_set
+            y_one_hot_batch, y_logits_batch = dcg_module.cast_label_to_one_hot_and_prototype(
+                y_labels_batch, params)
+
+            n = x_batch.size(0)
+
+            # record unflattened x as input to guidance aux classifier
+            # the below 3 lines should be un-commented if we have model.arch in ["simple", "linear"]
+            # x_unflat_batch = x_batch.to(self.device)
+            # if params.data.dataset == "toy" or params.model.arch in ["simple", "linear"]:
+            #     x_batch = torch.flatten(x_batch, 1)
+
+            data_time += time.time() - data_start
+
+    ####################  Reverse diffusion code end   #############################
+
     if os.path.exists(report_file):
         os.remove(report_file)
     f = open(report_file, 'w')
     f.close()
-    
