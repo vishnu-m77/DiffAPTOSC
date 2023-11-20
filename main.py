a6517b08b909954815516f6c3550e82d59bd01b4
Lakshay <59942900+Lakshay-G@users.noreply.github.com>
2023-11-19 23:23:25 -0500

    Error while combining with diffusion.py

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ import src.dataloader.dataloader as dataloader
 import src.DCG.main as dcg_module
 import src.diffusion as diffusion
 
+from src.network.unet_model import *
+
 if os.path.exists('project.log'):
     os.remove('project.log')
 
@@ -96,6 +98,7 @@ if __name__ == '__main__':
     logging.info("Noised Variable is {}".format(noised_var))
 
     #################### Reverse diffusion code begins #############################
+    model = ConditionalModel(config=param, guidance=False)
 
     for epoch in range(0, 5):
 
@@ -106,17 +109,55 @@ if __name__ == '__main__':
 
             x_batch, y_labels_batch = feature_label_set
             y_one_hot_batch, y_logits_batch = dcg_module.cast_label_to_one_hot_and_prototype(
-                y_labels_batch, params)
+                y_labels_batch, param)
 
             n = x_batch.size(0)
 
             # record unflattened x as input to guidance aux classifier
             # the below 3 lines should be un-commented if we have model.arch in ["simple", "linear"]
+            x_unflat_batch = x_batch
             # x_unflat_batch = x_batch.to(self.device)
-            # if params.data.dataset == "toy" or params.model.arch in ["simple", "linear"]:
+            # if param.data.dataset == "toy" or param.model.arch in ["simple", "linear"]:
             #     x_batch = torch.flatten(x_batch, 1)
 
             data_time += time.time() - data_start
+
+            model.train()
+
+            dcg.eval()
+
+            # step += 1
+            # antithetic sampling
+            num_timesteps = param["diffusion"]["timesteps"]
+            t = torch.randint(low=0, high=num_timesteps,
+                              size=(n // 2 + 1,))
+            t = torch.cat([t, num_timesteps - 1 - t], dim=0)[:n]
+
+            # noise estimation loss
+            # x_batch = x_batch.to(self.device)
+            # y_0_batch = y_logits_batch.to(self.device)
+            y_0_hat_batch, y_0_global, y_0_local = dcg(x_unflat_batch)
+            y_0_hat_batch = y_0_hat_batch.softmax(dim=1)
+            y_0_global, y_0_local = y_0_global.softmax(
+                dim=1), y_0_local.softmax(dim=1)
+
+            # print(y_0_hat_batch.size(), y_0_global.size(), y_0_local.size())
+
+            y_T_mean = y_0_hat_batch
+            # if config.diffusion.noise_prior:  # apply 0 instead of f_phi(x) as prior mean
+            #     y_T_mean = torch.zeros(y_0_hat_batch.shape).to(y_0_hat_batch.device)
+            y_0_batch = y_one_hot_batch.to(self.device)
+            e = torch.randn_like(y_0_batch).to(y_0_batch.device)
+            y_t_batch = q_sample(y_0_batch, y_T_mean,
+                                 self.alphas_bar_sqrt, self.one_minus_alphas_bar_sqrt, t, noise=e)
+            y_t_batch_global = q_sample(y_0_batch, y_0_global,
+                                        self.alphas_bar_sqrt, self.one_minus_alphas_bar_sqrt, t, noise=e)
+            y_t_batch_local = q_sample(y_0_batch, y_0_local,
+                                       self.alphas_bar_sqrt, self.one_minus_alphas_bar_sqrt, t, noise=e)
+            # output = model(x_batch, y_t_batch, t, y_T_mean)
+            output = model(x_batch, y_t_batch, t, y_0_hat_batch)
+            output_global = model(x_batch, y_t_batch_global, t, y_0_global)
+            output_local = model(x_batch, y_t_batch_local, t, y_0_local)
 
     ####################  Reverse diffusion code end   #############################
 
