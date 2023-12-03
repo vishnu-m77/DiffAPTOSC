a63c8bcc8654a41a4e437011acf5405724cc7e97
Lakshay <59942900+Lakshay-G@users.noreply.github.com>
2023-12-03 18:08:51 -0500

    T-SNE implementation done

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.metrics import f1_score
+from sklearn.manifold import TSNE
 
 
 def plot_loss(loss_arr, title, xlabel, ylabel, savedir):
@@ -31,3 +32,16 @@ def compute_f1_score(target, pred):
     # pred_np = np.argmax(pred_np, axis=1)
     F1 = f1_score(target, pred_np, average='macro')
     return F1
+
+
+def t_sne(y_t, t_num):
+    y_t = np.array(y_t)
+    y_t_tsne = TSNE(
+        n_components=2, perplexity=1.5).fit_transform(y_t)
+    # print(y_t_tsne)
+    # Plot the t-SNE visualization with color-coded classes
+    plt.scatter(y_t_tsne[:, 0], y_t_tsne[:, 1], c=range(
+        0, len(y_t_tsne[:, 1])), cmap='viridis', marker='o', edgecolors='w', s=100)
+    plt.title('t-SNE Visualization of 5 Classes at t='+str(t_num))
+    plt.savefig('t_sne_'+str(t_num)+'.png', format='PNG')
+    plt.close()
