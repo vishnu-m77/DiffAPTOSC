8179b3d914a0e24721e3a405d7a93477bb7c5b97
vishnu-m77 <vmugunda@uwaterloo.ca>
2023-12-12 10:22:19 -0500

    Implemented weighted_loss as a class

--- src/diffusion.py
+++ src/diffusion.py
@@ -208,16 +208,22 @@ def compute_mmd(x, y):
     mmd = x_kernel + y_kernel - 2*xy_kernel
     return mmd
 
-def weight_modif(y, loss_vector, weights = [1/100,1,1,1,1], weighted_loss = False):
-    if weighted_loss:
-        weight_list = []
-        logging.info(y)
-        for label in y:
-            label = weights[label]
-            weight_list.append(label)
-        loss_vector = torch.sum(loss_vector, dim=1)
-        loss_vector = loss_vector*torch.tensor(weight_list)
-    return loss_vector
+class weighted_loss():
+    def __init__(self, y, weights = [1/100,1,1,1,1], weighted_loss = False):
+        self.y = y
+        self.weights = weights
+        self.weighted_loss = weighted_loss
+    
+    def loss(self, loss_vector):
+        if self.weighted_loss:
+            weight_list = []
+            logging.info(self.y)
+            for label in self.y:
+                label = self.weights[label]
+                weight_list.append(label)
+            loss_vector = torch.sum(loss_vector, dim=1)
+            loss_vector = loss_vector*torch.tensor(weight_list)
+        return loss_vector
     
 def weight_wrapper(y, weights = [1/100,1,1,1,1], weighted_loss = False):
     """
@@ -262,9 +268,10 @@ def get_loss(x,y, params, dcg, FD, model):
     output = model(x, yt_fusion, t, dcg_fusion)
     output_global = model(x, yt_global, t, dcg_global)
     output_local = model(x, yt_local, t, dcg_local)
-    loss = weight_modif(y, (eps - output).square()).mean() + 
-            0.5*(weight_modif(y, compute_mmd(eps, output_global)).mean() + 
-            weight_modif(y, compute_mmd(eps, output_local)).mean())
+    
+    weighted = weighted_loss(y=y)
+    loss = weighted.loss((eps - output).square()).mean() + 0.5*(weighted.loss(compute_mmd(eps, output_global)).mean() + 
+                                                      weighted.loss(compute_mmd(eps, output_local)).mean())
             
     # weight_function = weight_wrapper(y=y)
     # loss = weight_function((eps - output).square()).mean() + 0.5*(weight_function(compute_mmd(eps, output_global)).mean() + 
