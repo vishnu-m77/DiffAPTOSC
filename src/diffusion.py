22a10fcb9a4e71855f5e4651e0e5d01837659a20
Sehmimul <sehmimul777@gmail.com>
2023-12-13 19:20:38 -0500

    clean: cleaned diffusion script, removed unnecessary get_out function and removed unnecessary local and global priors form dcg in one place

--- src/diffusion.py
+++ src/diffusion.py
@@ -56,7 +56,6 @@ class DiffusionBaseUtils():
         if t < 1:
             raise ValueError(
                 "Invalid timestep. Timestep must be at least 1 to obtain reverse diffusion parameters")
-        # will throw error at time step t = 0 MAKE SURE TO DEAL WITH IT
         alpha_prod_t_m1 = self.get_alpha_prod(timestep=t-1)
 
         gamma_0 = beta_t*(torch.sqrt(alpha_prod_t_m1)/(1 - alpha_prod_t))
@@ -80,9 +79,9 @@ class ForwardDiffusion(DiffusionBaseUtils):
         obtain the respective noisy variables following equation 2 of DiffMIC paper.
         y_0, global and local priors will be obtained from dcg.
 
-        t is the timestep till which noise has been added in the forward diffusion process
+        - t is the timestep till which noise has been added in the forward diffusion process
 
-        var is the variable on which we are adding noise and it needs to be a float tensor
+        - var is the variable on which we are adding noise and it needs to be a float tensor
 
         Note:
         * prior = (global_prior + local_prior)/2 for y_0
@@ -106,36 +105,9 @@ class ReverseDiffusion(DiffusionBaseUtils):
         super(ReverseDiffusion, self).__init__(
             config=config
         )
-        """
-        NOTES: - Sehmimul - Notes not removed as reverse diffusion notes may be useful in debugging reverse diffusion
 
-        1. The diffusion pipeline in DiffMIC is based off https://arxiv.org/abs/2206.07275 
-        Github: https://github.com/XzwHan/CARD/blob/ebe2f2ae95dc7a7a95e6a71c0c8e1cabf8451087/classification/diffusion_utils.py#L106
-        Especially the CARD paper's Algorithm 2 line 4 is very useful as thta talks about inference and
-        the same inference scheme has been applied in the DiffMIC paper
-        Thus CARD has to be an intergal part of our work. Output of p_sample is literally the denoising
-        process of the CARD paper.
-
-        2. I understand p_sample, but I don't understand the goal of p_sample_t_1to0. The goal of y_0_reparam is outlined in point 1
-
-        3.  p_sample is just reverse process of CARD paper and it is just equation 9 and Algorithm 2 and it
-
-        4. extract function is not necessary and the x or y input is only required to get the 'shape' and it is not really useful otherwise
-
-        5. In p_sample_loop curl_y is just a sample drawn from the N(prior, I) distribution.
-        """
-
-    # cannot test without cond_prior and score_net
     def reverse_diffusion_step(self, x, y_t, t, cond_prior, score_net):
-        """
-        This is similar to p_sample of code
-        x: The input image which will be used in the Score Network
-        y_t: noisy variable at a specific timestep t
-        t: timestep at which we are doing doing reverse diffusion and t will go from T to 1
-        cond_prior: prior for local or global prior or local+global prior and according to CARD it should depend on x
-        score_net: Neural Network which is used to approximate the gradient of the log likelihood of the probability distribution
-        """
-        # First calculate the time dependent parameters gamma_0, gamme_1, gamma_2 and beta_var
+        # First we calculate the time dependent parameters gamma_0, gamme_1, gamma_2 and beta_var
         # In reverse diffusion, at each timestep t, we are essentially sampling from a Gaussian Distribution
         # whose mean is defined using gamma_0, gamma_1 and gamma_2 and the variance is defined by beta_var
         # Note that gamma_0, gamma_1, gamma_2, beta_var depend on the timestep of reverse diffusion
@@ -144,7 +116,7 @@ class ReverseDiffusion(DiffusionBaseUtils):
         eps = torch.randn_like(y_t)
         t = torch.tensor([t])
 
-        # first we reparameterize y0 to obtain y0_hat
+        # Now we reparameterize y0 to obtain y0_hat
         y0_hat = (1/torch.sqrt(alpha_prod_t))*(y_t - (1-torch.sqrt(alpha_prod_t)) *
                                                cond_prior - torch.sqrt(1-alpha_prod_t)*score_net.forward(x, y_t, t, yhat=cond_prior))
 
@@ -153,7 +125,6 @@ class ReverseDiffusion(DiffusionBaseUtils):
 
         return y_tm1, y0_hat
 
-    # cannot test without cond_prior and score_net
     def full_reverse_diffusion(self, x, cond_prior, score_net, t1, t2, t3):
         """
         This does the full reverse diffusion process. We start by initializing a random sample from a Gaussian Distribution
@@ -165,7 +136,7 @@ class ReverseDiffusion(DiffusionBaseUtils):
             t = self.T - t - 1
             if t > 0:
                 y_tm1, y0_hat = self.reverse_diffusion_step(
-                    x, y_t, t, cond_prior, score_net)  # method will crash if t = 0
+                    x, y_t, t, cond_prior, score_net) 
                 y_t = y_tm1
 
                 if t == t1:
@@ -175,7 +146,6 @@ class ReverseDiffusion(DiffusionBaseUtils):
                 elif t == t3:
                     y_t_3 = y_tm1
             else:
-                # so t = 1
                 y_tm1 = y0_hat
 
         y0_synthetic = y_tm1
@@ -183,18 +153,24 @@ class ReverseDiffusion(DiffusionBaseUtils):
 
 
 def compute_kernel(x, y):
+    """
+    Helper function to compute MMD loss
+    """
     x_size = x.size(0)
     y_size = y.size(0)
     dim = x.size(1)
-    x = x.unsqueeze(1)  # (x_size, 1, dim)
-    y = y.unsqueeze(0)  # (1, y_size, dim)
+    x = x.unsqueeze(1)  
+    y = y.unsqueeze(0) 
     tiled_x = x.expand(x_size, y_size, dim)
     tiled_y = y.expand(x_size, y_size, dim)
     kernel_input = (tiled_x - tiled_y).pow(2).mean(2)/float(dim)
-    return torch.exp(-kernel_input)  # (x_size, y_size)
+    return torch.exp(-kernel_input)
 
 
 def compute_mmd(x, y):
+    """
+    Computes MMD Loss
+    """
     x_kernel = compute_kernel(x, x)
     y_kernel = compute_kernel(y, y)
     xy_kernel = compute_kernel(x, y)
@@ -203,6 +179,9 @@ def compute_mmd(x, y):
 
 
 class weighted_loss():
+    """
+    Class used to compute weighted loss
+    """
     def __init__(self, y, weight = "n"):
         self.y = y
         w = [1/1805, 1/370, 1/999, 1/193, 1/295]
@@ -231,6 +210,9 @@ class weighted_loss():
 
 
 def get_loss(x, y, params, dcg, FD, model):
+    """
+    Helper function to compute loss
+    """
     y0, _ = dcg.cast_label_to_one_hot_and_prototype(y)
     n = x.size(0)
     num_timesteps = params["timesteps"]
@@ -258,6 +240,9 @@ def get_loss(x, y, params, dcg, FD, model):
 
 
 def train(dcg, model, params, train_loader, val_loader):
+    """
+    Main training function which contains training loop
+    """
     FD = ForwardDiffusion(config=params)  # initialize class
 
     optimizer = torch.optim.Adam(
@@ -307,25 +292,13 @@ def train(dcg, model, params, train_loader, val_loader):
               val_loss_array=loss_batch_val, mode="diffusion")
 
 
-def get_out(dcg, model, feature_label_set, reverse_diffusion):
-    x_batch, y_labels_batch = feature_label_set
-    dcg_fusion, dcg_global, dcg_local = dcg.forward(x_batch)
-    dcg_fusion = dcg_fusion.softmax(dim=1)  # the actual label
-    y_T_mean = dcg_fusion
-    y_out = reverse_diffusion.full_reverse_diffusion(
-        x_batch, cond_prior=y_T_mean, score_net=model)
-    logging.info("Actual: {}, DCG_out: {}, Diff_out: {}".format(
-        y_labels_batch, torch.argmax(y_T_mean, dim=1), torch.argmax(y_out.softmax(dim=1), dim=1)))
-    return y_out.softmax(dim=1)
-
 
 def eval(dcg, model, params, test_loader):
+    """
+    Main eval function which contains evaluation loop
+    """
     t1, t2, t3 = params["t_sne"]["t1"], params["t_sne"]["t2"], params["t_sne"]["t3"]
     reverse_diffusion = ReverseDiffusion(config=params)
-    # outputs = Parallel(n_jobs=-1)(delayed(self.one_object_pred)(df.loc[df['object_id'] == object], object, report_file, verbose) for object in objects)
-
-    # Parallel/ delayed code calls get_out which does the job of the for loop following it. Only one of the two should be active at any given time
-    # outputs = Parallel(n_jobs=-1)(delayed(get_out)(dcg, model, feature_label_set, reverse_diffusion) for i, feature_label_set in enumerate(test_loader))
     targets = []
     dcg_output = []
     diffusion_output = []
@@ -337,7 +310,7 @@ def eval(dcg, model, params, test_loader):
     model.eval()
     for i, feature_label_set in enumerate(test_loader):
         x_batch, y_labels_batch = feature_label_set
-        dcg_fusion, dcg_global, dcg_local = dcg.forward(x_batch)
+        dcg_fusion, _, _ = dcg.forward(x_batch)
         dcg_fusion = dcg_fusion.softmax(dim=1)  # the actual label
         y_T_mean = dcg_fusion
         y_out, y_out_1, y_out_2, y_out_3 = reverse_diffusion.full_reverse_diffusion(
